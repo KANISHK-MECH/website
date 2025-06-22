145ac77575f85cb20e09addccd5c79818fe7bbb4
KANISHK.R <136042521+KANISHK-MECH@users.noreply.github.com>
2025-06-22 22:47:15 +0530

    Remove All Certification Data

---
+++ supabase/migrations/20250622171708_square_star.sql
@@ -0,0 +1,14 @@
+/*
+  # Remove all certification data
+
+  1. Data Cleanup
+    - Delete all existing certification records
+    - Clear the certifications table completely
+  
+  2. Purpose
+    - Clean slate for certifications section
+    - Remove all previously added certification data
+*/
+
+-- Delete all certifications from the database
+DELETE FROM certifications;
